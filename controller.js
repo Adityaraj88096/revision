dee6cc58167caaca12ff0341140d14c9a58161d3
Adityaraj88096 <adityaraj88096@gmail.com>
2025-04-10 09:06:00 +0530

    Removed objectId in query

--- controller.js
+++ controller.js
@@ -47,18 +47,19 @@ testCtrl.getAllPapersByExamType = async (req, res) => {
 // This route is I was working on previous night
 testCtrl.getPaperByExamId = async (req, res) => {
     const {body, exam, id} = req.params;
+    console.log(`Exam = ${exam}, Body = ${body}, Id = ${id}`);
     try {
         if(body == 'upsc') {
-        const data = await upsc.find({ exam: exam, _id: ObjectId(id) });
+        const data = await upsc.find({ exam: exam, _id: id });
         res.status(200).send(data);
         } else if(body == 'cat'){
-            const data = await cat.find({ exam: exam, _id: ObjectId(id) });
+            const data = await cat.find({ exam: exam, _id: id });
             res.status(200).send(data);
         } else if(body == 'bank') {
-            const data = await bank.find({ exam: exam, _id: ObjectId(id) });
+            const data = await bank.find({ exam: exam, _id: id });
             res.status(200).send(data);
         } else if(body == 'ssc') {
-            const data = await ssc.find({ exam: exam, _id: ObjectId(id) });
+            const data = await ssc.find({ exam: exam, _id: id });
             res.status(200).send(data);
         }
     } catch (error) {
